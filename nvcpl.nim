b3c4e6030dcddb30f27bc0eb205610d1ba883414
Aetopia <kirbymario22@gmail.com>
2022-10-18 20:58:02 +0530

    Fix detection.

--- nvcpl.nim
+++ nvcpl.nim
@@ -4,10 +4,13 @@ setCurrentDir(getAppDir())
 if not isAdmin(): 
     discard shellExecutew(0, newWideCString("runas"), newWideCString(getAppFilename()), nil, nil, 5)
     quit(0)
-var svc = GetObject("winmgmts:").ExecQuery("SELECT * FROM Win32_Service WHERE Name='NVDisplay.ContainerLocalSystem'").ItemIndex(0)
+var (wmi, query) = (GetObject("winmgmts:"), "SELECT * FROM Win32_Service WHERE Name='NVDisplay.ContainerLocalSystem'")
+var svc = wmi.ExecQuery(query).ItemIndex(0)
 svc.ChangeStartMode("Manual")
 svc.StartService()
 svc.ChangeStartMode("Disabled")
-while svc.State == "OK": discard
+while true: 
+    sleep(1)
+    if wmi.ExecQuery(query).ItemIndex(0).State == "Running": break
 discard waitForExit(startProcess("nvcplui.exe"), -1)
 svc.StopService()
